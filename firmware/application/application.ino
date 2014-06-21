126fbc13c339721d7b3f3484c61ba92d4cb42509
Matthew O'Gorman <mog@rldn.net>
2014-06-21 07:21:48 -0500

    more packet traces

--- firmware/application/application.ino
+++ firmware/application/application.ino
@@ -16,10 +16,24 @@ uint32_t time;
 uint8_t sequence;
 uint8_t door_state, check_state;
 
-uint8_t lock_door[] = {0xbd, 0x04, 0x0c, 0xe7, 0x03, 0x13};
-uint8_t unlock_door[] = {0xbd, 0x04, 0x09, 0xe7, 0x05, 0x10};
-uint8_t status_door[] = {0xbd, 0x04, 0x07, 0xe7, 0x07, 0x1c};
-uint8_t message[6];
+uint8_t lock_door[] =     {0xbd, 0x04, 0x0c, 0xe7, 0x03, 0x13};
+uint8_t unlock_door[] =   {0xbd, 0x04, 0x09, 0xe7, 0x05, 0x10};
+uint8_t status_door[] =   {0xbd, 0x04, 0x07, 0xe7, 0x07, 0x1c};
+uint8_t first_message[] = {0xbd,0x05,0x02,0xaa,0x18,0x01,0x4b};
+
+
+void send_message(uint8_t *message, int length)
+{
+  int i;
+      message[2] = sequence;
+      message[length - 1] = 0xff;
+      for(i = 1; i < length - 1; i++) {
+  	message[length -1] ^= message[i];
+      }
+      for(i = 0 ; i < length; i++) {
+  	Serial1.write(message[i]);
+      }
+}
 
 void setup()
 {
@@ -34,47 +48,21 @@ void loop()
 {
   uint8_t input, loop, i;
   loop = 0;
-  //  sequence = 0x0c;
-  if(check_state) {
-    check_state = 0;
-      status_door[2] = sequence;
-      status_door[5] = 0xff;
-      for(i = 1; i < 5; i++) {
-	status_door[5] ^= status_door[i];
-      }
-      for(i = 0 ; i < 6; i++) {
-	//Serial.print(lock_door[i], HEX);
-	//      Serial.print(":");
-	Serial1.write(status_door[i]);
-      }
-  } else {
-    check_state = 1;
-    /* if(!door_state) { */
-    /*   door_state = 1;//fix */
-    /*   lock_door[2] = sequence; */
-    /*   lock_door[5] = 0xff; */
-    /*   for(i = 1; i < 5; i++) { */
-    /* 	lock_door[5] ^= lock_door[i]; */
-    /*   } */
-    /*   for(i = 0 ; i < 6; i++) { */
-    /* 	//Serial.print(lock_door[i], HEX); */
-    /* 	//      Serial.print(":"); */
-    /* 	Serial1.write(lock_door[i]); */
-    /*   } */
-    /*   Serial.print("\r\n"); */
-    /* } else { */
-    /*   door_state = 0; */
-    /*   unlock_door[2] = sequence; */
-    /*   unlock_door[5] = 0xff; */
-    /*   for(i = 1; i < 5; i++) { */
-    /* 	unlock_door[5] ^= unlock_door[i]; */
-    /*   } */
-    /*   for(i = 0 ; i < 6; i++) { */
-    /* 	Serial1.write(unlock_door[i]); */
-    /*   } */
-    /* } */
-  }
-  sequence++;
+  /* if(check_state) { */
+  /*   check_state = 0; */
+  /*   send_message(status_door,6); */
+  /* } else { */
+  /*   check_state = 1; */
+  /*   if(!door_state) { */
+  /*     door_state = 1;//fix */
+  /*     send_message(lock_door,6); */
+  /*     Serial.print("\r\n"); */
+  /*   } else { */
+  /*     door_state = 0; */
+  /*     send_message(unlock_door,6); */
+  /*   } */
+  /* } */
+  /* sequence++; */
   while (Serial1.available()) {
     if (!loop) {
       Serial.print(time);
@@ -83,16 +71,17 @@ void loop()
       Serial.print(":\t");
     }
     input = Serial1.read();
+    Serial.print("0x");
     if(input < 16) Serial.print("0");
     Serial.print(input, HEX);
-    Serial.print(":");
+    Serial.print(" ");
     loop = 1;
   }
   if (loop) {
     Serial.println("");
   }
   time++;
-  delay(100);
+  delay(1000);
 }
 
 
