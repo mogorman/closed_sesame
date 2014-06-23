d4c19e222ee8678fcf95dec058674ec85e614c26
Matthew O'Gorman <mog@rldn.net>
2014-06-23 01:28:31 -0500

    working from 0 i think.  still dont know how to properly ack the status messages sent by zwave controller but other than that i think it all works.

--- firmware/application/application.ino
+++ firmware/application/application.ino
@@ -10,29 +10,65 @@
 #include <avr/power.h>
 #include <avr/wdt.h>
 #include <avr/pgmspace.h>
-#include <util/atomic.>
 
 uint32_t time;
 uint8_t sequence;
 uint8_t door_state, check_state;
 
-uint8_t lock_door[] =     {0xbd, 0x04, 0x0c, 0xe7, 0x03, 0x13};
-uint8_t unlock_door[] =   {0xbd, 0x04, 0x09, 0xe7, 0x05, 0x10};
-uint8_t status_door[] =   {0xbd, 0x04, 0x07, 0xe7, 0x07, 0x1c};
-uint8_t first_message[] = {0xbd,0x05,0x02,0xaa,0x18,0x01,0x4b};
+uint8_t lock[] =     {0xbd, 0x04, 0x0c, 0xe7, 0x03, 0x13};
+uint8_t unlock[] =   {0xbd, 0x04, 0x09, 0xe7, 0x05, 0x10};
+uint8_t status[] =   {0xbd, 0x04, 0x07, 0xe7, 0x07, 0x1c};
 
+//uint8_t pair_0[] =    {0xbd, 0x04, 0x01, 0xe7, 0x18, 0x05};
+uint8_t unpair[] =   {0xbd, 0x04, 0x03, 0xe7, 0x0e, 0x11};
 
-void send_message(uint8_t *message, int length)
+uint8_t add_code[] = {0xBD, 0x0A, 0x0F, 0xE7, 0x0B, 0x00, 0x01, 0x12, 0x34, 0x56, 0x78, 0x1F};
+uint8_t delete_code[] = {0xbd, 0x06, 0x1c, 0xe7, 0x0d, 0x00, 0x01, 0x0c};
+uint8_t check_code[]={0xBD, 0x06, 0x10, 0xE7, 0x0C, 0x00, 0x01, 0x03};
+
+uint8_t pair_0[] =    {0xbd, 0x04, 0x01, 0xe7, 0x18, 0x05};
+uint8_t pair_1[] =   {0xbd, 0x04, 0x08, 0xe7, 0x09, 0x1d};
+uint8_t pair_2[] =   {0xBD, 0x0B, 0x09, 0xE7, 0x0F, 0x14, 0x06, 0x21, 0x03, 0x33, 0x52, 0x06, 0x42};
+uint8_t pair_3[] =   {0xBD, 0x04, 0x0A, 0xE7, 0x10, 0x06};
+uint8_t pair_4[] =   {0xBD, 0x0B, 0x0B, 0xE7, 0x0F, 0x14, 0x06, 0x20, 0x20, 0x33, 0x54, 0x05, 0x67}; 
+//uint8_t first_message[] = {0xbd,0x05,0x02,0xaa,0x18,0x01,0x4b};
+
+void check_code_message(uint8_t slot)
 {
-  int i;
+  check_code[7] = slot;
+  send_message(check_code);
+}
+
+void delete_code_message(uint8_t slot)
+{
+  check_code[7] = slot;
+  send_message(delete_code);
+}
+
+void set_code_message(uint8_t *code,uint8_t slot)
+{
+  add_code[7] = slot;
+  send_message(add_code);
+}
+
+void send_message(uint8_t *message)
+{
+  uint8_t i;
+  uint8_t length = message[1] + 2;
       message[2] = sequence;
       message[length - 1] = 0xff;
       for(i = 1; i < length - 1; i++) {
   	message[length -1] ^= message[i];
       }
+      Serial.print(" sending ");
       for(i = 0 ; i < length; i++) {
+	Serial.print("0x");
+	Serial.print(message[i], HEX);
+	Serial.print(" ");
   	Serial1.write(message[i]);
       }
+      Serial.println("");
+      sequence++;
 }
 
 void setup()
@@ -50,19 +86,62 @@ void loop()
   loop = 0;
   /* if(check_state) { */
   /*   check_state = 0; */
-  /*   send_message(status_door,6); */
+  /*   send_message(status); */
   /* } else { */
   /*   check_state = 1; */
   /*   if(!door_state) { */
   /*     door_state = 1;//fix */
-  /*     send_message(lock_door,6); */
+  /*     send_message(lock); */
   /*     Serial.print("\r\n"); */
   /*   } else { */
   /*     door_state = 0; */
-  /*     send_message(unlock_door,6); */
+  /*     send_message(unlock); */
   /*   } */
   /* } */
   /* sequence++; */
+  while (Serial.available()) {
+    input = Serial.read();
+    switch(input) {
+    case 'u':
+      Serial.print("unlock");
+      send_message(unlock);
+      break;
+    case 'l':
+      Serial.print("lock");
+      send_message(lock);
+      break;
+    case 's':
+      Serial.print("status");
+      send_message(status);
+      break;
+    case 'p':
+      Serial.print("unpair");
+      send_message(unpair);
+      break;
+    case '0':
+      Serial.print("pair_0");
+      send_message(pair_0);
+      break;
+    case '1':
+      Serial.print("pair_1");
+      send_message(pair_1);
+      break;
+    case '2':
+      Serial.print("pair_2");
+      send_message(pair_2);
+      break;
+    case '3':
+      Serial.print("pair_3");
+      send_message(pair_3);
+      break;
+    case '4':
+      Serial.print("pair_4");
+      send_message(pair_4);
+      break;
+    default:
+      break;
+    }
+  }
   while (Serial1.available()) {
     if (!loop) {
       Serial.print(time);
